c7dcce0f1b23ab869b29ae253fc453cdc155f828
Andrew Chambers <ac@acha.ninja>
2020-01-10 20:30:47 +1300

    Add graceful(ish) shutdown.

--- cmd/poolparty/main.go
+++ cmd/poolparty/main.go
@@ -1,19 +1,18 @@
 package main
 
-
 import (
 	"fmt"
 	"os"
-	"time"
-	"strings"
+	"os/signal"
 	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/andrewchambers/poolparty"
-	"github.com/valyala/fasthttp"
-	flag "github.com/spf13/pflag"
 	log "github.com/inconshreveable/log15"
+	flag "github.com/spf13/pflag"
+	"github.com/valyala/fasthttp"
 )
-
 
 type fasthttpLogAdaptor struct {
 	l log.Logger
@@ -23,7 +22,7 @@ func (l *fasthttpLogAdaptor) Printf(format string, args ...interface{}) {
 	l.l.Info(fmt.Sprintf(format, args...))
 }
 
-func main () {
+func main() {
 	log.Root().SetHandler(log.StderrHandler)
 
 	workerRequestTimeout := flag.Duration("worker-request-timeout", 60*time.Second, "timeout before a worker is considered crashed")
@@ -37,9 +36,9 @@ func main () {
 	flag.Parse()
 
 	cfg := poolparty.PoolConfig{
-		Logger: log.New(),
-		NumWorkers: *poolSize,
-		WorkerProc: flag.Args(),
+		Logger:               log.New(),
+		NumWorkers:           *poolSize,
+		WorkerProc:           flag.Args(),
 		WorkerRequestTimeout: *workerRequestTimeout,
 	}
 
@@ -50,7 +49,6 @@ func main () {
 		os.Exit(1)
 	}
 	defer pool.Close()
-
 
 	var staticFileHandler fasthttp.RequestHandler
 
@@ -64,8 +62,8 @@ func main () {
 		log.Info("serving static files", "dir", staticDirPath)
 		staticFileHandler = fasthttp.FSHandler(staticDirPath, 1)
 	}
-	
-	handler := func (ctx *fasthttp.RequestCtx) {
+
+	handler := func(ctx *fasthttp.RequestCtx) {
 		startt := time.Now()
 		id := fmt.Sprintf("%d", ctx.ID())
 		log := log.New("id", id)
@@ -82,8 +80,8 @@ func main () {
 
 		resp, err := pool.Dispatch(poolparty.JanetRequest{
 			RequestID: id,
-			Headers: string(ctx.Request.Header.RawHeaders()),
-			Body: string(ctx.Request.Body()),
+			Headers:   string(ctx.Request.Header.RawHeaders()),
+			Body:      string(ctx.Request.Body()),
 		})
 		log.Info("janet request finished", "duration", time.Now().Sub(startt))
 		if err != nil {
@@ -96,18 +94,35 @@ func main () {
 		ctx.SetBody([]byte(resp.Body))
 	}
 
-
 	server := &fasthttp.Server{
-		ReadTimeout: *readTimeout,
-		WriteTimeout: *writeTimeout,
-		Concurrency: *requestBacklog,
+		ReadTimeout:        *readTimeout,
+		WriteTimeout:       *writeTimeout,
+		Concurrency:        *requestBacklog,
 		MaxRequestBodySize: *maxRequestBodySize,
-		Logger: &fasthttpLogAdaptor{log.Root()},
-		Handler: handler,
-		ReduceMemoryUsage: true,
+		Logger:             &fasthttpLogAdaptor{log.Root()},
+		Handler:            handler,
+		ReduceMemoryUsage:  true,
 	}
 
+	gracefulShutdown := make(chan struct{}, 1)
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		<-c
+		signal.Reset(os.Interrupt)
+		log.Info("got shutdown signal, shutting down")
+		server.Shutdown()
+		close(gracefulShutdown)
+	}()
+
 	err = server.ListenAndServe(":8080")
-	log.Error("server stopped", "err", err)
-	os.Exit(1)
+	if err != nil {
+		log.Error("server stopped", "err", err)
+		os.Exit(1)
+	}
+	<-gracefulShutdown
+	log.Info("shutting down worker pool")
+	pool.Close()
+	log.Info("graceful shutdown complete")
 }
